d2adb5fd03c413b7f963b2a56c6bd5e1d81bca4b
harishb2006 <imharishba@gmail.com>
2025-08-21 11:42:18 +0530

    f calling

--- server/controller/queryController.js
+++ server/controller/queryController.js
@@ -1,5 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { zeroShotPrompt } from "../utils/prompts/zeroShotPrompt.js";
+import { tryFunctionCall } from "../utils/functions.js";
 
 
 
@@ -8,6 +9,10 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 export const queryPDF = async (req, res) => {
   try {
     const { question } = req.body;
+    const { called, result } = await tryFunctionCall(question, pdfMeta);
+    if (called) {
+      return res.json({ result });
+    }
 
     const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
     const prompt = zeroShotPrompt(question);
